aecb88197b5d168df53f4ace8646ae8f664bf216
Jonathan Kashongwe <jokashongwe@gmail.com>
2023-10-04 11:19:10 +0100

    Add pv

--- src/Controller/ResultatController.php
+++ src/Controller/ResultatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BureauVoteRepository;
+use App\Repository\CandidatRepository;
 use App\Repository\ResultatRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,6 +34,43 @@ class ResultatController extends AbstractController
             'rapport' => $total,
             'rapport_bv' => json_encode(array_values($totalParBureauVote)),
             'bureau_votes' => json_encode(array_keys($totalParBureauVote))
+        ]);
+    }
+    
+    #[Route('/resultat/pvs/{id}', name: 'app_resultat_pv')]
+    public function pv(
+        ResultatRepository $resultatRepository, 
+        CandidatRepository $candidatRepository, 
+        $id,
+        BureauVoteRepository $bureauVoteRepository
+    ): Response
+    {
+        $candidat = $candidatRepository->find($id);
+        if(is_null($candidat)){
+            $this->addFlash('error', "Identifiant du candidat non valide");
+            return $this->redirectToRoute('app_candidat');
+        }
+        $resultats = $resultatRepository->findBy(['candidat' => $id]);
+        $all_pv = [];
+        foreach($resultats as $resultat){
+            $pv = $resultat->getProceVerbaux();
+            $codeBV  = $resultat->getCodeBV();
+            $bureau_vote = $bureauVoteRepository->findOneBy(['code' => $codeBV]);
+            $address =  "";
+            if(!is_null($bureau_vote)){
+                $address = $bureau_vote->getAddress();
+            }
+            $codeBV = "$address  $codeBV";
+            if(!empty($pv)){
+                array_push($all_pv, [$codeBV => $pv]);
+            }
+        }
+
+        
+        return $this->renderForm('resultat/pv.html.twig', [
+            'controller_name' => 'ResultatController',
+            'all_pv' => $all_pv,
+            'candidat_id' => $id 
         ]);
     }
 }
