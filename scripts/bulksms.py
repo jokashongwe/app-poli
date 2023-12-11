0fe5641db12b16de35e4ce2d3f6bf42dc269ab69
Jonathan Kashongwe <jokashongwe@gmail.com>
2023-12-11 16:20:14 +0100

    Fix values

--- scripts/bulksms.py
+++ scripts/bulksms.py
@@ -36,7 +36,7 @@ def get_phones_from_group(group: str) -> List[str]:
             .replace(")", "")
         )
         numbers.append(parsed_phone)
-        if len(numbers) == 20:
+        if len(numbers) == 100:
             yield numbers
             numbers = []
     yield numbers
